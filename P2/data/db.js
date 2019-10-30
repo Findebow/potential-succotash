9f2ea36ee19e1838266ca974f8df34e9cda9c808
arnabjorg17 <34029748+arnabjorg17@users.noreply.github.com>
2019-10-30 15:42:17 +0000

    Data

--- P2/data/db.js
+++ P2/data/db.js
@@ -1,16 +1,17 @@
 
  const mongoose = require('mongoose');
+ const reviewSchema = require('../schemas/review');
 
-// VANTAR MEIRA
-
-/*
-const connection = mongoose.createConnection('mongodb://', {
+const connection = mongoose.createConnection('mongodb+srv://honnunogsmidi:honnunogsmidi@cluster0-oy581.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-*/
+const Review = connection.model('Review', reviewSchema);
+
 module.exports = {
-  //connection
+  connection,
+  Review
+  
 };
 
---
+++ P2/data/populateDb.js
@@ -0,0 +1,15 @@
+var publications = require('./publications.json');
+var friends = require('./friends.json');
+const mongoDb = require('mongodb');
+const MongoClient = mongoDb.MongoClient;
+
+const connectionString = "mongodb+srv://honnunogsmidi:honnunogsmidi@cluster0-oy581.mongodb.net/test?retryWrites=true&w=majority";
+
+const dbName = "honnunogsmidi";
+
+
+
+
+
+
+// EFTIR AÐ KLÁRA
